4cfc62147dcb935fb4e44dda9dfafb57c10967fe
timruiterkamp <timruiterkamp@gmail.com>
2018-09-29 22:51:59 +0200

    added tags filter

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 🚀 Work in progress 🚀  
 The setup of a basic webscraper build on NodeJS with [puppeteer](https://github.com/GoogleChrome/puppeteer) (Headless chrome browser).  
-Right now it is set to Medium articles, this can be changed if you search for the right DOM elements.
+Right now it is set to Hackernoon articles, this can be changed if you search for the right DOM elements on your desired source.
 
 ## Idea
 
--- scraper.js
+++ scraper.js
@@ -4,28 +4,27 @@ const fs = require('fs');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    // this url can be changed
-    // if it is changed, you need to update the query selectors beneath.
-    const url = 'https://medium.com/topic/technology';
+    const url = 'https://hackernoon.com/tagged/software-development';
+    const tags = ['data science', 'machine learning', 'javascript'];
 
     await page.goto(url);
-    // TODO: 
-    // h3 is way too general, need an concrete DOM element
+
     const articles = await page.evaluate(() => 
-        Array.from(document.querySelectorAll('h3'))
-        .map(streamItem => ({
-            title: streamItem.textContent.trim(),
-            url: streamItem.querySelector('a').href
+        
+        Array.from(document.querySelectorAll('div.postArticle'))
+        .filter((article) => {
+            return article.querySelector('h3').textContent.toLowerCase().includes('javascript');
+        })
+        .map((article) => ({
+            title: article.querySelector('h3').textContent.trim(),
+            url: article.querySelector('a').href
         }))
         
     );
-
-    // TODO / IDEAS:
-    // output file to an index file
-    // add to array when new items come in
-    // Setup front-end to use the data
     
+    console.log(articles);
     let data = JSON.stringify(articles);
     fs.writeFileSync('data.json', data);
+
     await browser.close();
 })();
